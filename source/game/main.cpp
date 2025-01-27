5b199b5b3cfb2b3bd008b3705d3d115b59efecad
Remi LAOT <remilaot@gmail.com>
2025-01-27 15:10:41 +0100

    link to raylib

--- source/game/main.cpp
+++ source/game/main.cpp
@@ -2,11 +2,53 @@
 #include <string>
 
 #include "lib.hpp"
+#include "raylib.h"
+
+constexpr auto SCREEN_WIDTH = 800;
+constexpr auto SCREEN_HEIGHT = 450;
 
 auto main() -> int
 {
-  auto const lib = library {};
-  auto const message = "Hello from " + lib.name + "!";
-  std::cout << message << '\n';
-  return 0;
+    auto const lib = library {};
+    auto const message = "Hello from " + lib.name + "!";
+    std::cout << message << '\n';
+
+    // Initialization
+    //--------------------------------------------------------------------------------------
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);  // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())  // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Congrats! You created your first window!",
+                 190,
+                 200,
+                 20,
+                 LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();  // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
 }
